e97adc843d6eb1703b6f80166682df4b7527ac66
swbcwang <104905623+swbcwang@users.noreply.github.com>
2022-06-23 18:37:55 +0800

    Add files via upload

---
+++ include/wxj.h
@@ -0,0 +1,49 @@
+# ifndef _next_
+# define _next_
+void Next();
+# endif
+
+# ifndef _kmp_
+# define _kmp_
+int kmp();
+# endif
+
+# ifndef _single_ada_
+# define _single_ada_
+int single_ada();
+# endif
+
+# ifndef _double_ada_
+# define _double_ada_
+int double_ada();
+# endif
+
+# ifndef _per_wrong_
+# define _per_wrong_
+int per_wrong();
+# endif
+
+# ifndef _per_in_
+# define _per_in_
+int per_in();
+# endif
+
+# ifndef _per_del_
+# define _per_del_
+int per_del();
+# endif
+
+# ifndef _del_poly_
+# define _del_poly_
+int del_poly();
+# endif
+
+# ifndef _single_pri_
+# define _single_pri_
+int single_pri();
+# endif
+
+# ifndef _double_pri_
+# define _double_pri_
+int double_pri();
+# endif
