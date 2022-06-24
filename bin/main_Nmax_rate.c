859eeb89bfa2526b446bc28bd76877f0be72087a
LiangY1 <104839849+LiangY1@users.noreply.github.com>
2022-06-24 11:37:44 +0800

    Add files via upload

---
+++ bin/main_Nmax_rate.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "Pattern_select.h"
+#include "CountN.h"
+#include "SiteN.h"
+
+int main(int argc,char** argv){
+    char seq[100];
+	strcoy(seq,argv[1]); //DNA序列
+    double rate=atof(argv[2]); //允许保留的最大N的比例
+    int len=strlen(seq);
+    int num=len*rate;//允许保留的最大N的数目
+    int *AddrN;
+    int NumN=CountN(seq,num);
+    if(NumN!=0){
+    AddrN=(int*)malloc(sizeof(int)*NumN);
+    SiteN(seq,AddrN);
+    printf("the origin seq: %s\n",seq);
+    Pattern_select(seq,AddrN,NumN,num);
+    printf("%s\n",seq);}
+    return 0;
+}
